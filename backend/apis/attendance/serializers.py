5f493c83adbcbb71d5633579e22cad52f3ac3768
afsalat <afsalat9@gmail.com>
2025-06-17 11:45:21 +0530

    fix - bug fixing

--- backend/apis/attendance/serializers.py
+++ backend/apis/attendance/serializers.py
@@ -2,9 +2,10 @@ from rest_framework import serializers
 from .models import Attendance
 
 
-
 class AttendanceSerializer(serializers.ModelSerializer):
+    fullname = serializers.CharField(source='user.fullname', read_only=True)
 
     class Meta:
         model = Attendance
-        fields = '__all__'
+        fields = ['id', 'user', 'fullname', 'date', 'checkin', 'checkout', 'location',
+                  'status', 'work_report', 'salary_cut', 'validation']
